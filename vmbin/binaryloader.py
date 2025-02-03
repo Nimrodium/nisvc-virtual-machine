87d9a37648f0a68ddbeabf9cdac506877982d7af
Nimrodium <nimrodium@protonmail.com>
2025-02-02 21:35:03 -0600

    mov and movim works ! ! might rename movim to something like loadimm idk movim sounds stupid

---
+++ vmbin/binaryloader.py
@@ -0,0 +1,22 @@
+def assemble(name:str,data:list[int],program:list[int]):
+    with open(name, "wb") as f:
+        # Write signature
+        f.write(b"NISVC-EF")
+        f.write((len(data)).to_bytes(8, byteorder='little'))
+        f.write((len(program)).to_bytes(8, byteorder='little'))
+        f.write(bytes(data))
+        f.write(bytes(program))
+
+
+data = []
+program = [
+    0x02,0x00,  # movim
+    0x01,0x00,  # r1
+    0x08,0x0a,0x0a,    # u16 2570
+    0x01,0x00,  # mov
+    0x02,0x00,  # r2
+    0x01,0x00,  # r1
+    0xFF,0xFF   # end
+]
+
+assemble("test.bin",data,program)
